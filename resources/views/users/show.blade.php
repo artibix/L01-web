3d1b59233344570d3b939246264469957093835b
artibix <artibix@proton.me>
2023-06-16 22:08:06 +0800

    修复样式问题

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,7 +6,9 @@
     <div class="offset-md-2 col-md-8">
         <div class="col-md-12">
             <div class="offset-md-2 col-md-8">
-                @include('shared._user_info', ['user' => $user])
+                <section class="user_info">
+                    @include('shared._user_info', ['user' => $user])
+                </section>
             </div>
         </div>
     </div>
