549fce9efb31824ab3b2a4fab7017f4d2fbb1099
artibix <artibix@proton.me>
2023-06-17 10:58:17 +0800

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,15 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
+/* 注册 */
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
+
+/* 显示登陆界面 */
+Route::get('login', 'SessionsController@create')->name('login');
+/* 创建新会话 */
+Route::post('login', 'SessionsController@store')->name('login');
+/* 注销 */
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
+
