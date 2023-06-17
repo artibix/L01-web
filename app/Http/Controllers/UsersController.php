96c8dcc75b9c0d1067c58be031468e3dc204ebec
artibix <artibix@proton.me>
2023-06-17 15:26:54 +0800

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
 
 class UsersController extends Controller
 {
@@ -35,5 +37,41 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
+    }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => [
+                'required',
+                Rule::unique('users')->ignore($user->id),
+                'max:50',
+            ],
+            'password' => 'nullable|confirmed|min:6',
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+
+        if (Hash::check($request->password, $user->password)) {
+            session()->flash('danger', '请勿使用旧密码');
+            return redirect()->back();
+        }
+
+        if ($request->password)
+        {
+            $data['password'] = bcrypt($request->password);
+        }
+
+        $user->update($data);
+
+        session()->flash('success', '更新用户信息成功！');
+
+        return redirect()->route('users.show', $user);
     }
 }
