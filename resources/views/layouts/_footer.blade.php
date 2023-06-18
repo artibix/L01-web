8300f8e31957f8ec039552f876993c5b6eff6cf1
artibix <artibix@proton.me>
2023-06-18 18:35:26 +0800

    发布微博

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,9 +1,5 @@
 <footer class="footer">
-  <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
-  <a href="https://learnku.com/laravel/courses" target=_blank class="text-decoration-none">
-    刻意练习，每日精进
-  </a>
-
+  <span>小小微博</span>
   <div class="float-end">
     <a href="{{ route('about') }}" class="text-decoration-none">关于</a>
   </div>
--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,5 @@ Route::post('password/email', 'PasswordController@sendResetLinkEmail')->name('pa
 Route::get('password/reset/{token}', 'PasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'PasswordController@reset')->name('password.update');
 
+/* 微博 */
+Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
