8e983e1cbcb1e04563096ce3a4f47ae5eba761c2
artibix <artibix@proton.me>
2023-06-18 18:16:55 +0800

    用户微博列表

--- resources/views/shared/_user_info.blade.php
+++ resources/views/shared/_user_info.blade.php
@@ -1,6 +1,6 @@
 <a href="{{ route('users.show', $user->id) }}">
 {{--    <img src="{{ $user->gravatar('140') }}" , alt="{{ $user->name }}" class="gravatar">--}}
-主页
+用户头像，暂未施工
 </a>
 
 <h1>{{ $user->name }}</h1>
